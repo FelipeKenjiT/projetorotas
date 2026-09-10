6c38f6b537cd1479c9bd01347a3df5fd5d1656d4
Felipe Kenji Takata <lipe.takata@gmail.com>
2026-09-10 09:33:01 -0300

    feat: Criando as rotas do projeto

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Link to="/">Home</Link>
+      <Link to="sobre">Sobre</Link>
+      <Link to="/produto">Produto</Link>
+      <Link to="/contato">Contato</Link>
+    </>
   )
 }
 
